0d43aecde2e45a400064c6384727d3410a6bc690
Gift Sinthong <psinthon@uci.edu>
2018-02-12 14:14:41 -0800

    update test script

--- lib/hello.java
+++ lib/hello.java
@@ -7,14 +7,9 @@ public class hello{
 		jep.eval("import sys");
 		jep.eval("import numpy");
 		jep.eval("import scipy");
+		jep.eval("import sklearn")
 		jep.eval("import pickle");
 		jep.eval("print(\'Hello from Python\')");
-		jep.eval("f =  open(\'sentiment_pipeline\', \'rb\')");
-		jep.eval("print(f)");
-		//jep.eval("d = pickle.loads(f, \'latin1\')");
-		//jep.eval("f.close()");
-		//jep.eval(" print(d.predict([\'I like it\']))");
-		jep.eval("f.close()");
 		System.out.println("Hello from Java");
 	}
 }
